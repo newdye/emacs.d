e64ee201cef413763ce1be05fd55a16b4b07a789
newdye <hi.wszk@aliyun.com>
2016-10-11 23:58:50 +0800

    add elpa to under git control

---
+++ elpa/spinner-1.7.1/spinner-autoloads.el
@@ -0,0 +1,15 @@
+;;; spinner-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("spinner.el") (22525 2476 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; spinner-autoloads.el ends here
