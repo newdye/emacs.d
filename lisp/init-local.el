c19416f69b151c13e7cee72112fdd41194647b6c
zhang kai <zhang kai>
2016-10-18 09:29:26 +0800

    add gtags configure in init-local.el

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,4 +1,4 @@
-;; (add-to-list 'load-path' "C:/Program Files/git/bin")
-;; (setenv "PATH" (concat "C:\\Program Files\\git/bin;" (getenv "PATH") ) )
 (add-to-list 'exec-path "C:/Program Files/git/bin")
+(add-to-list 'exec-path "D:/32.emacs/GnuGlobal/bin/")
+(add-to-list 'Info-additional-directory-list "D:/32.emacs/GnuGlobal/share/info")
 (provide 'init-local)
