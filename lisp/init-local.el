7bc6813f2c4e75c7d6f9f1537148fc8c5753c7ef
newdye <hi.wszk@aliyun.com>
2016-11-17 00:47:03 +0800

    normal commit

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,6 +1,8 @@
 (add-to-list 'exec-path "C:/Program Files/git/bin")
 (add-to-list 'exec-path "D:/32.emacs/tools/GnuGlobal/bin/")
 (add-to-list 'Info-additional-directory-list "D:/32.emacs/tools/GnuGlobal/share/info")
+(add-to-list 'Info-additional-directory-list "D:/32.emacs/HOME/.emacs.d/elpa/sicp-20151130.757")
+(add-to-list 'load-path "D:/32.emacs/emacs-c-ide-demo")
 (setq tramp-default-method "plink")
 (setq password-cache-expiry "3600000")
 ;; ;; init.el comment init-ido.el & init-ivy.el disable ido-mode, if want to use
@@ -9,15 +11,18 @@
 ;; ;; (setq ido-use-filename-at-point 'guess)
 ;; ;; (setq ido-everywhere t)
 ;; ;; (ido-mode t)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-              (ggtags-mode 1))))
+;; (global-set-key (kbd "M-x") 'smex)
+;; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+;; configure in c ide demo
+;; (add-hook 'c-mode-common-hook
+;;           (lambda ()
+;;             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+;;               (ggtags-mode 1))))
 (setq url-proxy-services
       '(("no_proxy" . "^\\(localhost\\|10.*\\)")
         ("http" . "proxy.zte.com.cn:80")
         ("https" . "proxy.zte.com.cn:80")))
+(require-package 'ggtags)
+(require 'init-c-ide)
 (provide 'init-local)
