2d32885d4e52ddbbd11c6f6ef582d59c373ce881
newdye <hi.wszk@aliyun.com>
2017-06-15 09:53:32 +0800

    add epla

---
+++ elpa-24.5/cmd-to-echo-20161203.1333/cmd-to-echo-pkg.el
@@ -0,0 +1,1 @@
+(define-package "cmd-to-echo" "20161203.1333" "Show the output of long-running commands in the echo area" '((emacs "24.4") (s "1.11.0") (shell-split-string "20151224.208")))
---
+++ elpa-24.5/elpa/csv-mode-1.6/csv-mode-autoloads.el
@@ -0,0 +1,15 @@
+;;; csv-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("csv-mode.el") (22525 1042 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; csv-mode-autoloads.el ends here
